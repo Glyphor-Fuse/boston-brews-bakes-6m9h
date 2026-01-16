0ec6e9f4b3cca39cd212a187c86c6ca83261246d
glyphor-ai <glyphor-ai@noreply.github.com>
2026-01-16 16:15:50 +0000

    ai: repair - build (attempt 1)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Index from './pages/Index';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Index />} />
-      </Routes>
-    </BrowserRouter>
+    <Index />
   );
 }
 
